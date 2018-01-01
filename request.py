684392ddbdad46ad41f6e666951dcff1a100477d
goomba-86 <olli.a.sjoberg@gmail.com>
2018-01-01 22:47:20 +0200

    Created data request message

---
+++ request.py
@@ -0,0 +1,8 @@
+
+class Request:
+    
+    def __init__(self, forward, reverse, left, right):
+        self.forward = forward
+        self.reverse = reverse
+        self.left = left
+        self.right = right
